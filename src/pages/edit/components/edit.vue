19b33cddfd01d4f2bb8307ee65be4db5abe1518f
strugglinglee <strugglinglee@163.com>
2021-12-16 11:03:15 +0800

    add

---
+++ src/pages/edit/components/edit.vue
@@ -0,0 +1,28 @@
+<template>
+  <view>
+    <movable-area class="area">
+      <movable-view class="move" style="" direction="all">带我走</movable-view>
+    </movable-area>
+  </view>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style>
+.area {
+  height: 200px;
+  width: 200px;
+  background: red;
+}
+.move {
+  height: 50px;
+  width: 50px;
+  background: blue;
+}
+</style>
