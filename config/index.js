261c30455c04664a95eba258f2377711067f3af7
strugglinglee <strugglinglee@163.com>
2022-01-13 10:07:23 +0800

    fix: 项目配置优化

--- config/index.js
+++ config/index.js
@@ -1,70 +1,71 @@
 const config = {
-  projectName: 'painter',
-  date: '2021-11-26',
-  designWidth: 750,
-  deviceRatio: {
-    640: 2.34 / 2,
-    750: 1,
-    828: 1.81 / 2
-  },
-  sourceRoot: 'src',
-  outputRoot: 'dist',
-  plugins: [],
-  defineConstants: {
-  },
-  copy: {
-    patterns: [
-    ],
-    options: {
-    }
-  },
-  framework: 'vue3',
-  mini: {
-    postcss: {
-      pxtransform: {
-        enable: true,
-        config: {
-
-        }
-      },
-      url: {
-        enable: true,
-        config: {
-          limit: 1024 // 设定转换尺寸上限
-        }
-      },
-      cssModules: {
-        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
-        config: {
-          namingPattern: 'module', // 转换模式，取值为 global/module
-          generateScopedName: '[name]__[local]___[hash:base64:5]'
-        }
-      }
-    }
-  },
-  h5: {
-    publicPath: '/',
-    staticDirectory: 'static',
-    postcss: {
-      autoprefixer: {
-        enable: true,
-        config: {
-        }
-      },
-      cssModules: {
-        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
-        config: {
-          namingPattern: 'module', // 转换模式，取值为 global/module
-          generateScopedName: '[name]__[local]___[hash:base64:5]'
-        }
-      }
-    }
-  }
+    projectName: 'painter',
+    date: '2021-11-26',
+    designWidth: 750,
+    deviceRatio: {
+        640: 2.34 / 2,
+        750: 1,
+        828: 1.81 / 2,
+    },
+    alias: {
+        '@/components': path.resolve(__dirname, '..', 'src/components'),
+        '@/utils': path.resolve(__dirname, '..', 'src/utils'),
+        '@/hooks': path.resolve(__dirname, '..', 'src/hooks'),
+        '@/constants': path.resolve(__dirname, '..', 'src/constants'),
+        '@/meta': path.resolve(__dirname, '..', 'src/meta'),
+    },
+    sourceRoot: 'src',
+    outputRoot: 'dist',
+    plugins: [],
+    defineConstants: {},
+    copy: {
+        patterns: [],
+        options: {},
+    },
+    framework: 'vue3',
+    mini: {
+        postcss: {
+            pxtransform: {
+                enable: true,
+                config: {},
+            },
+            url: {
+                enable: true,
+                config: {
+                    limit: 1024, // 设定转换尺寸上限
+                },
+            },
+            cssModules: {
+                enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+                config: {
+                    namingPattern: 'module', // 转换模式，取值为 global/module
+                    generateScopedName: '[name]__[local]___[hash:base64:5]',
+                },
+            },
+        },
+    },
+    h5: {
+        publicPath: '/',
+        staticDirectory: 'static',
+        postcss: {
+            autoprefixer: {
+                enable: true,
+                config: {},
+            },
+            cssModules: {
+                enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+                config: {
+                    namingPattern: 'module', // 转换模式，取值为 global/module
+                    generateScopedName: '[name]__[local]___[hash:base64:5]',
+                },
+            },
+        },
+    },
 }
 
 module.exports = function (merge) {
-  if (process.env.NODE_ENV === 'development') {
-    return merge({}, config, require('./dev'))
-  }
-  return merge({}, config, require('./prod'))
+    if (process.env.NODE_ENV === 'development') {
+        return merge({}, config, require('./dev'))
+    }
+    return merge({}, config, require('./prod'))
 }
